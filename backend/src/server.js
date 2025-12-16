2a77908527ca9afea56791bc9f2b25dc806c27eb
farihaferdous-sudo <fariha.ferdous@g.bracu.ac.bd>
2025-12-16 14:57:52 +0600

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// middleware
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT);
+    });
+});
+
